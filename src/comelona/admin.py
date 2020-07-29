7b7eedcbd209e67040ac2d5f2038a9a418cf8c3e
Jordy-Pedro-Valencia-Jara <jquispejar@unsa.edu.pe>
2020-07-29 18:57:28 -0500

    version 6

--- src/comelona/admin.py
+++ src/comelona/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import platillo
+from .models import chef
 # Register your models here.
 admin.site.register(platillo)
+admin.site.register(chef)
--- src/comelona/models.py
+++ src/comelona/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class chef(models.Model):
+    name=models.CharField(max_length=100)
+    las_name=models.CharField(max_length=100)
+    img=models.ImageField(upload_to='pics')
+    email=models.EmailField()
 class platillo(models.Model): 
     name=models.CharField(max_length=100)
     img=models.ImageField(upload_to='pics')
     desc=models.TextField()
     Brev=models.CharField(max_length=100)
     price=models.IntegerField()
+    chef_cocinero=models.ForeignKey(chef,on_delete=models.CASCADE,blank=False,null=True)
+
+
+
