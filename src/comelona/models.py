d1837094541b37eb952ee0eea4df23fc1b97be96
diegodavidpe <64116797+diegodavidpe@users.noreply.github.com>
2020-08-10 11:43:23 -0500

    modelo comelona

--- src/comelona/models.py
+++ src/comelona/models.py
@@ -13,6 +13,8 @@ class platillo(models.Model):
     Brev=models.CharField(max_length=100)
     price=models.IntegerField()
     chef_cocinero=models.ForeignKey(chef,on_delete=models.CASCADE,blank=False,null=True)
+    img=models.ImageField(upload_to='pics')
+    desc=models.TextField()
 
 
 
